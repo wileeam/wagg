5370c8b9f3a1e83cb5f0a214c40f1953ad44ea6c
Guillermo Rodriguez Cano <wschutz@gmail.com>
2015-10-24 00:35:03 +0200

    Restoring correct lifetime constants

--- lib/wagg/utils/constants.rb
+++ lib/wagg/utils/constants.rb
@@ -11,14 +11,14 @@ module Wagg
       COMMENT_RE = /(?<author>\w+):\s(?<timestamp>\d{1,2}\/\d{1,2}-\d{1,2}:\d{1,2}:\d{1,2})\skarma:\s(?<weight>-?\d+)/
       COMMENT_VOTES_QUERY_URL = 'https://www.meneame.net/backend/get_c_v.php?id=%{id}&p=%{page}'
       COMMENT_URL = 'https://www.meneame.net/c/%{comment}'
-      COMMENT_VOTES_LIFETIME = 30*24*60*60 + 24*60*60 # 30 days
+      COMMENT_VOTES_LIFETIME = 30*24*60*60 # 30 days
       # News's vote regular expression and URL query templates
       # author: HH:MM TMZ valor: #
       # author: DD-MM-YYYY HH:MM TMZ valor: #
       NEWS_RE = /(?<author>.+):\s(?<timestamp>(\d{1,2}-\d{1,2}-\d{4}\s)?\d{1,2}:\d{1,2})\s([A-Z]+)\svalor:\s(?<weight>-?\d+)/
       NEWS_VOTES_QUERY_URL = 'https://www.meneame.net/backend/meneos.php?id=%{id}&p=%{page}'
-      NEWS_CONTRIBUTION_LIFETIME = 30*24*60*60 + 24*60*60 # 30 days
-      NEWS_VOTES_LIFETIME = 30*24*60*60 + 24*60*60 # 30 days
+      NEWS_CONTRIBUTION_LIFETIME = 30*24*60*60 # 30 days
+      NEWS_VOTES_LIFETIME = 30*24*60*60 # 30 days
       # Page URL query templates
       PAGE_URL = 'https://www.meneame.net/?page=%{page}'
       # Vote regular expression matching both votes for news and comments (Not perfect but rather accurate)
