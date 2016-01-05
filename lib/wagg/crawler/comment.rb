81aaeba8710cfc4184bcaa5f4ac7f0933885d9aa
Guillermo Rodriguez Cano <wschutz@gmail.com>
2016-01-05 02:48:12 +0100

    [FIX] Attempting to fix a regression on encoding detection in comment's body parsing

--- lib/wagg/crawler/comment.rb
+++ lib/wagg/crawler/comment.rb
@@ -97,7 +97,11 @@ module Wagg
           # Parse comment's body data
           body_item = item.search('.//div[contains(concat(" ", normalize-space(@class), " "), " comment-body ")]')
           #comment_body = body_item.search('./child::node()').to_s.scrub.strip
-          comment_body = body_item.inner_html.strip.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+          if body_item.inner_html.encoding != Encoding::UTF_8
+            comment_body = body_item.inner_html.strip.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+          else
+            comment_body = body_item.inner_html.strip
+          end
           comment_id = Wagg::Utils::Functions.str_at_xpath(body_item, './@id')[/(?!c-)(?<id>\d+)/].to_i
           # Also available at unique id: //*[@id="cid-XXXXXXXX"]/a/@href
           # TODO: Use regex to remove last element in extraced href instead of these functions...
